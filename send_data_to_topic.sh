c17e186fd675ee43cb9a32b4f3b1b1f9737c7bff
Daniel McCoy Stephenson <dmccoystephenson@gmail.com>
2023-06-22 13:36:37 -0600

    Added kafkacat installation check to `send_data_to_topic.sh`.

--- send_data_to_topic.sh
+++ send_data_to_topic.sh
@@ -7,6 +7,15 @@ elif [ -z "$DEPOSIT_TOPIC" ]; then
     exit 1
 fi
 
+# check if kafkacat is installed
+if ! command -v kafkacat &> /dev/null
+then
+    # install kafkacat
+    echo "kafkacat could not be found. Installing kafkacat."
+    sudo apt-get update
+    sudo apt-get install -y kafkacat
+fi
+
 # send "test" message to topic
 echo "Sending test message to topic $DEPOSIT_TOPIC."
 kafkacat -b $DOCKER_HOST_IP:9092 -t $DEPOSIT_TOPIC -P -p 0 -K: <<< "test: test"
